edad5f1b0a64e0bee38ce41c29554742ada80179
thiele <thiele.carto@gmail.com>
2023-01-05 17:22:23 -0300

    Initial commit

---
+++ script/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    $('#form-task').on('submit', (e) => {
+        e.preventDefault();
+        addTask();
+        done();
+    })
+
+    let tasksList = [];
+
+    function addTask() {
+        const task = $('#input-task').val();
+        if (tasksList.includes(task)) {
+            alert('Esta tarefa já está cadastrada!')
+        } else {
+            tasksList.push(task);
+            const taskItem = $(`<li></li>`).text(task.toUpperCase());
+            taskItem.appendTo('#list');
+        }
+    }
+
+    function done(){
+        $("#list li").click(function() {
+        $(this).addClass("done");
+    })
+    }
+})
+
